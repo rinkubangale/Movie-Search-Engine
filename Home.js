505bd7c8d18b51c644ec7aa06c88d60b6d8fd5f5
Rinku Bangale <84958228+rinkubangale@users.noreply.github.com>
2021-07-28 20:35:33 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -26,30 +26,44 @@ async function getData() {
         
         let div = document.createElement("div")
 
-        let p_title = document.createElement("div");
-        p_title.innerHTML = el.Title;
+        if (el.Response == "False") {
+            
+            let img = document.createElement("div");
+            img.innerHTML = el.Error;
+            img.style.marginTop = "100px"
+
+            let p_title = document.createElement("img");
+            p_title.src = "https://media.istockphoto.com/vectors/error-icon-vector-illustration-vector-id922024224?k=6&m=922024224&s=612x612&w=0&h=LXl8Ul7bria6auAXKIjlvb6hRHkAodTqyqBeA6K7R54=";
+            
+            div.append(img, p_title);
+            movies_div.append(div);
+            
+        } else {
+
+            let p_title = document.createElement("div");
+            p_title.innerHTML = el.Title;
     
-        let p_rating = document.createElement("div");
-        p_rating.innerHTML = "IMDB: " + el.imdbRating + "⭐";
+            let p_rating = document.createElement("div");
+            p_rating.innerHTML = "IMDB: " + el.imdbRating + "⭐";
 
-        let img = document.createElement("img");
-        img.src = el.Poster;
+            let img = document.createElement("img");
+            img.src = el.Poster;
 
-        let p_date = document.createElement("div");
-        p_date.innerHTML = el.Released;
+            let p_date = document.createElement("div");
+            p_date.innerHTML = el.Released;
 
-        let disc = document.createElement("div");
-        disc.innerHTML = el.Plot
-        disc.style.fontSize = "15px";
-        disc.style.justifyContent = "center";
-        disc.style.margin = "5px";
-        disc.style.color = "grey";
+            let disc = document.createElement("div");
+            disc.innerHTML = el.Plot
+            disc.style.fontSize = "15px";
+            disc.style.justifyContent = "center";
+            disc.style.margin = "5px";
+            disc.style.color = "grey";
 
 
-        div.append(img, p_title, disc, p_date, p_rating);
+            div.append(img, p_title, disc, p_date, p_rating);
 
-        movies_div.append(div);
-
+            movies_div.append(div);
+        }
 
     }
 
